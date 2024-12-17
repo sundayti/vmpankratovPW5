0b09b6b72d37caea6fc8eb262a066424db41c0db
sundayti <pankratovvlad1@gmail.com>
2024-12-17 23:37:38 +0300

    Add configureTableView in NewsViewController

--- vmpankratovPW5/NewsScene/NewsViewController.swift
+++ vmpankratovPW5/NewsScene/NewsViewController.swift
@@ -17,19 +17,38 @@ final class NewsViewController: UIViewController, NewsDisplayLogic {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
 
-        // Do any additional setup after loading the view.
+}
+
+extension NewsViewController: UITableViewDataSource, UITableViewDelegate {
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.pinTop(to: view.topAnchor)
+        tableView.pinLeft(to: view.leadingAnchor)
+        tableView.pinRight(to: view.trailingAnchor)
+        tableView.pinBottom(to: view.bottomAnchor)
+        
+        tableView.register(NewsCell.self, forCellReuseIdentifier: "NewsCell")
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return displayArticles.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath) as! NewsCell
+        let displayArticle = displayArticles[indexPath.row]
+        cell.configure(with: displayArticle)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let article = interactor?.articles[indexPath.row] else { return }
+        router?.routeToArticleDetail(with: article)
+    }
 }
