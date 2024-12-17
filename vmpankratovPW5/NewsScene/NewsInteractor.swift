11cca568c763b1afa3ce082debcbc6eac4c807d7
sundayti <pankratovvlad1@gmail.com>
2024-12-17 11:35:22 +0300

    Create class NewsInteractor

--- vmpankratovPW5/NewsScene/NewsInteractor.swift
+++ vmpankratovPW5/NewsScene/NewsInteractor.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+final class NewsInteractor: NewsBusinessLogic, NewsDataStore {
+    var presenter: NewsPresentationLogic?
+    var worker: NewsWorker = NewsWorker()
+    var articles: [ArticleModel] = []
+    
+    func loadNews(_ request: News.Load.Request) {
+        worker.fetchNews(
+            rubric: request.rubricId,
+            pageIndex: request.pageIndex
+        ) { [weak self] result in
+            switch result {
+            case .success(let fetchedArticles):
+                self?.articles = fetchedArticles
+                let response = News.Load.Response(articles: fetchedArticles)
+                self?.presenter?.presentNews(response)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
