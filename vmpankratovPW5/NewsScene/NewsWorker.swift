b8bf7ac2ce8883094192e614417428cf7f9fac84
sundayti <pankratovvlad1@gmail.com>
2024-12-17 11:26:24 +0300

    Add strucr NewsPage in NewsWorker

--- vmpankratovPW5/NewsScene/NewsWorker.swift
+++ vmpankratovPW5/NewsScene/NewsWorker.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+final class NewsWorker {
+    private let decoder: JSONDecoder = JSONDecoder()
+    
+    struct NewsPage: Decodable {
+        enum CodingKeys: String, CodingKey {
+            case news
+            case requestId
+        }
+        
+        var news: [ArticleModel]?
+        var requestID: String?
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            news = try container.decodeIfPresent([ArticleModel].self, forKey: .news)
+            requestID = try container.decodeIfPresent(String.self, forKey: .requestId)
+            
+            if let reqID = requestID, let unwrappedNesws = news {
+                news = unwrappedNesws.map {
+                    var newArticle = $0
+                    newArticle.requestId = reqID
+                    return newArticle
+                }
+            }
+        }
+    }
+}
