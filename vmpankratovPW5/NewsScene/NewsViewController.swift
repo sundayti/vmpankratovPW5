7c94c25bf485e3e888b780950f7de269063d47b2
sundayti <pankratovvlad1@gmail.com>
2024-12-17 10:23:12 +0300

    Create NewsViewController

---
+++ vmpankratovPW5/NewsScene/NewsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewsViewController.swift
+//  vmpankratovPW5
+//
+//  Created by Tom Tim on 17.12.2024.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
